5d09b57b3e9d1e422080317a41c2d25d098c3262
macascarp <62967134+macascarp@users.noreply.github.com>
2021-02-17 16:49:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// Importamos mongoose, cors y express
 
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 
-// Importo el router AUTO
 const auto = require('./routes/auto')
 
-// Usar Express
 const app = express()
-// Usar corse como middleware
+
 app.use(cors())
-// Se manejara con json
+
 app.use(express.json())
-// Usar la URL /api/auto
+
 app.use("/api/auto", auto)
 
 const port = process.env.port || 3000
