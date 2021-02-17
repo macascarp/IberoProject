6fc940c6bea4d83768aab7266b1249b35bd74d88
macascarp <62967134+macascarp@users.noreply.github.com>
2021-02-17 16:48:04 +0000

    Update auto.js

--- routes/auto.js
+++ routes/auto.js
@@ -1,21 +1,14 @@
  const express = require('express')
 
- // Servicio de router en Express
  const router = express.Router()
 
- // Enlace a archivo AUTO
  const Auto = require('../model/auto')
-
- /* -------------------PRIMER PETICION---------------------- */
-
- // Cuando haga un GET en http://localhost:3000/listaAutos, me devolvera lo de abajo
- // Las promesas sirven para que  
+ 
 router.get('/listaAutos', async(req,res) => {
     const autos = await Auto.find()
     res.send(autos)
 })
 
-// Cuando haga un POST en http://localhost:3000/, pedira el formato de debajo
 
 router.post('/', async(req, res) => {
     const auto = new Auto ({
